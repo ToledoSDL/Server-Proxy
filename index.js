27d37aedd017f00d4bbf295c2a0b4f43a1db6a9d
ToledoSDL <lucastoledoaraujo@hotmail.com>
2022-08-16 22:36:37 -0300

    Adicionais

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-const redbird = require("redbird");
 const { readFileSync, writeFileSync, existsSync } = require("fs");
+const bodyParser = require("body-parser");
+const express = require("express");
+const redbird = require("redbird");
+
+const app = express();
+
+app.use(bodyParser.json({ strict: false }));
 
 if (!existsSync("./config.json")) {
 	console.error("File 'config.json' does not exist, creating...");
@@ -29,7 +35,33 @@ if (!existsSync("./config.json")) {
 	let config = JSON.parse(readFileSync("./config.json", "utf-8"));
 	const proxy = redbird({ port: config.proxyPort });
 
-	for (const { src, target } of config.urls) {
-		proxy.register(src, target);
+	for (const url of config.urls) {
+		proxy.register(url.src, url.target);
 	}
+
+	app.post("/api/v1/editProxy", (req, res) => {
+		if (req.body.oldSrc && req.body.oldTarget) {
+			try {
+				proxy.unregister(req.body.oldSrc, req.body.oldTarget);
+
+				proxy.register(req.body.newSrc, req.body.newTarget);
+				let actualConfig = JSON.parse(readFileSync("./config.json", "utf-8"));
+
+				actualConfig = actualConfig.filter((config) => config.src !== req.body.oldSrc && config.target !== req.body.oldTarget);
+
+				actualConfig.push({
+					src: req.body.newSrc,
+					target: req.body.newTarget,
+				});
+
+				res.json({ success: true });
+			} catch (error) {
+				res.json({ success: true, error });
+			}
+		} else {
+			res.json({ success: false });
+		}
+	});
 }
+
+app.listen(9999);
