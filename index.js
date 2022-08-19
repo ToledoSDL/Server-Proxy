d4be72ef0730ca020c8a8857463e7f0f84a5a812
ToledoSDL <lucastoledoaraujo@hotmail.com>
2022-08-19 17:55:50 -0300

    Atualizações

--- index.js
+++ index.js
@@ -2,66 +2,50 @@ const { readFileSync, writeFileSync, existsSync } = require("fs");
 const bodyParser = require("body-parser");
 const express = require("express");
 const redbird = require("redbird");
-
+const cors = require("cors");
+const httpProxy = require("http-proxy");
+const proxy = redbird({ port: config.proxyPort });
 const app = express();
+let config = JSON.parse(readFileSync("./config.json", "utf-8"));
+let proxies = [];
 
-app.use(bodyParser.json({ strict: false }));
+app.use(
+	cors({
+		origin: "*",
+	}),
+).use(bodyParser.json({ strict: false }));
 
-if (!existsSync("./config.json")) {
-	console.error("File 'config.json' does not exist, creating...");
-	writeFileSync(
-		"./config.json",
-		JSON.stringify(
-			{
-				proxyPort: 80,
-				urls: [
-					{
-						src: "domain.com",
-						target: "http://localhost:3000",
-					},
-					{
-						src: "subdomain.domain.com.br",
-						target: "http://localhost:3001",
-					},
-				],
-			},
-			null,
-			4,
-		),
-	);
-	console.info("File 'config.json' created, edit the information as you like.");
-	process.exit(1);
-} else {
-	let config = JSON.parse(readFileSync("./config.json", "utf-8"));
-	const proxy = redbird({ port: config.proxyPort });
-
-	for (const url of config.urls) {
+for (const url of config.urls) {
+	if (url.target.includes("localhost")) {
 		proxy.register(url.src, url.target);
+	} else {
+		proxies.push({ config: url, proxy: httpProxy.createProxyServer({ target: url.target }) });
 	}
-
-	app.post("/api/v1/editProxy", (req, res) => {
-		if (req.body.oldSrc && req.body.oldTarget) {
-			try {
-				proxy.unregister(req.body.oldSrc, req.body.oldTarget);
-
-				proxy.register(req.body.newSrc, req.body.newTarget);
-				let actualConfig = JSON.parse(readFileSync("./config.json", "utf-8"));
-
-				actualConfig = actualConfig.filter((config) => config.src !== req.body.oldSrc && config.target !== req.body.oldTarget);
-
-				actualConfig.push({
-					src: req.body.newSrc,
-					target: req.body.newTarget,
-				});
-
-				res.json({ success: true });
-			} catch (error) {
-				res.json({ success: true, error });
-			}
-		} else {
-			res.json({ success: false });
-		}
-	});
 }
+
+app.post("/api/v1/editProxy", (req, res) => {
+	if (req.body.oldSrc && req.body.oldTarget) {
+		try {
+			httpProxy.proxy.unregister(req.body.oldSrc, req.body.oldTarget);
+
+			proxy.register(req.body.newSrc, req.body.newTarget);
+			let actualConfig = JSON.parse(readFileSync("./config.json", "utf-8"));
+
+			actualConfig = actualConfig.filter((config) => config.src !== req.body.oldSrc && config.target !== req.body.oldTarget);
+
+			actualConfig.push({
+				src: req.body.newSrc,
+				target: req.body.newTarget,
+			});
+
+			res.json({ success: true });
+		} catch (error) {
+			console.log(error);
+			res.json({ success: true, error });
+		}
+	} else {
+		res.json({ success: false });
+	}
+});
 
 app.listen(9999);
